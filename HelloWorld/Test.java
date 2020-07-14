9fb0e429da5e56c7e2c687f1b9b775a6e6c96532
kiransatpute <kiransatpute52@gmail.com>
2020-07-14 20:30:32 +0530

    Update Test.java

--- HelloWorld/Test.java
+++ HelloWorld/Test.java
@@ -1,9 +1,9 @@
-package HelloWorld;
+ package HelloWorld;
 
 public class Test
 {
     public static void main(String[] args)
     {
-        System.out.println("Test");
+        System.out.println("Test kiran ");
     }
 }
